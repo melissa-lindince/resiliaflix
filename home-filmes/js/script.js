fc36e76b6b03312dfbd2eaf518091aea57b6f11a
anna <carolinanna93@gmail.com>
2021-11-02 02:19:02 -0300

    Adiciona CSS e carrossel na home de filmes

---
+++ home-filmes/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('.item').on('mouseover',function(e) {
+        const trailerUrl = $(this).find('img').attr('data-trailer')
+        
+        console.log(e)
+
+        $('.trailer').find('iframe').attr('src', trailerUrl)
+    })
+
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        dots: false,
+        margin: 3,
+        smartSpeed : 900,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1200: {
+                items: 6
+            }
+        },
+    });
+});
